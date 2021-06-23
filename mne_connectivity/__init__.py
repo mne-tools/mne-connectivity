7f63f9eebaf17297edff4b8dcab4040695bd0d48
Adam Li <adam2392@Adams-MBP-2.home>
2021-06-23 15:53:04 -0400

    Merging.

--- mne_connectivity/__init__.py
+++ mne_connectivity/__init__.py
@@ -1,16 +1,14 @@
 """Connectivity MEG, EEG, iEEG data processing."""
 
 # Authors: Adam Li <ali39@jhu.edu>
-#          Eric Larson <>
-#          Britta Westner <>
+#          Eric Larson <larson.eric.d@gmail.com>
+#          Britta Westner <britta.wstnr@gmail.com>
 #
 # License: BSD (3-clause)
 
-__version__ = "0.1.dev0"
+__version__ = '0.1'
 
 from .envelope import envelope_correlation
 from .effective import phase_slope_index
 from .spectral import spectral_connectivity
 from .utils import seed_target_indices, degree, check_indices
-
-__all__ = ['__version__']
