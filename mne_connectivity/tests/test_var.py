a0d28dfcde6719f44b7d70a6a375db70d08ce314
Eric Larson <larson.eric.d@gmail.com>
2021-09-15 15:43:40 -0400

    FIX: Spelling

--- mne_connectivity/tests/test_var.py
+++ mne_connectivity/tests/test_var.py
@@ -64,7 +64,7 @@ def create_noisy_data(
     return X.T, true_eigvals, A
 
 
-@pytest.mark.timout(15)
+@pytest.mark.timeout(15)
 def test_vector_auto_regression_computation():
     """Test VAR model computation accuracy.
 
