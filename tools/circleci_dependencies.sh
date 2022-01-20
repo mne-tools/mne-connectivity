20d47b03dd37a81fd1d80cc983ac58bab5cc892a
Eric Larson <larson.eric.d@gmail.com>
2022-01-20 09:49:17 -0500

    FIX: No no deps

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -18,7 +18,7 @@ python -m pip install --upgrade --progress-bar off --pre sphinx
 
 echo "Installing doc build dependencies"
 python -m pip uninstall -y pydata-sphinx-theme
-python -m pip install --upgrade --progress-bar off --only-binary matplotlib -r <(grep -Ev "mayavi|PySurfer" requirements.txt) -r requirements_testing.txt -r requirements_doc.txt
+python -m pip install --upgrade --progress-bar off --only-binary matplotlib -r requirements.txt -r requirements_testing.txt -r requirements_doc.txt
 python -m pip install --upgrade --progress-bar off https://github.com/mne-tools/mne-python/zipball/main
 python -m pip install --progress-bar off https://github.com/sphinx-gallery/sphinx-gallery/zipball/master https://github.com/pyvista/pyvista/zipball/main https://github.com/pyvista/pyvistaqt/zipball/main
 python -m pip install -e .
