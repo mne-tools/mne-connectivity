9ca7bd6427e4c57de1350a8d5e492d747a43bc51
Adam Li <adam2392@gmail.com>
2022-02-13 14:43:35 -0500

    Add extra workflow for conda install

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ URL = 'https://github.com/mne-tools/mne-connectivity'
 LICENSE = 'BSD-3'
 DOWNLOAD_URL = 'https://github.com/mne-tools/mne-connectivity'
 VERSION = version
-INSTALL_REQUIRES = ['numpy', 'scipy', 'mne', 'xarray', 'netCDF4', 'pandas']
+INSTALL_REQUIRES = ['numpy', 'scipy', 'mne', 'xarray', 'netCDF4', 'pandas', 'tqdm']
 CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Intended Audience :: Developers',
                'License :: OSI Approved',
