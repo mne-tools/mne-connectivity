6fe5abf91c15145893b87776f37560b669c8e2b6
Richard Koehler <richard.koehler@outlook.de>
2022-10-13 10:57:13 +0200

    Create epochs_multivar.py
    Initialize epochs_multivar

---
+++ mne_connectivity/spectral/epochs_multivar.py
@@ -0,0 +1,429 @@
+# Authors: Thomas S. Binns <>
+#          Mariia Mikhailenko <>
+#          Tien Dung Nguyen <>
+#          Veronika Shamova <>
+#          Orestis Sylianou <>
+#          Jeroen Habets <>
+#          Richard M. Köhler <koehler.richard@charite.de>
+#
+# License: BSD (3-clause)
+
+import inspect
+from functools import partial
+from typing import Union
+
+import numpy as np
+from mne import BaseEpochs
+from mne.epochs import BaseEpochs
+from mne.parallel import parallel_func
+from mne.source_estimate import _BaseSourceEstimate
+from mne.time_frequency.multitaper import (_compute_mt_params, _csd_from_mt,
+                                           _mt_spectra, _psd_from_mt,
+                                           _psd_from_mt_adaptive)
+from mne.time_frequency.tfr import cwt, morlet
+from mne.utils import _arange_div, _check_option, _time_mask, logger, warn
+from numpy.typing import ArrayLike, NDArray
+
+from ..base import SpectralConnectivity, SpectroTemporalConnectivity
+from ..utils import check_indices, fill_doc
+
+
+def multivar_spectral_connectivity_epochs(
+    data: Union[ArrayLike, BaseEpochs],
+    indices: tuple[tuple[ArrayLike]],
+    names: Union[list, None] = None,
+    method: Union[str, list[str]] = "mic",
+    sfreq: float = 6.283185307179586,
+    mode: str = "multitaper",
+    t0: float = 0.0,
+    tmin: Union[float, None] = None,
+    tmax: Union[float, None] = None,
+    fmt_fmin: float = 0.0,
+    fmt_fmax: float = np.inf,
+    cwt_freqs: Union[list[float], None] = None,
+    fmt_n_fft: Union[int, None] = None,
+    cwt_use_fft: bool = True,
+    mt_bandwidth: Union[float, None] = None,
+    mt_adaptive: bool = False,
+    mt_low_bias: bool = True,
+    cwt_n_cycles: Union[float, list[float]] = 7.0,
+    cwt_decim: Union[int, slice] = 1,
+    n_seed_components: Union[tuple[Union[int, None]], None] = None,
+    n_target_components: Union[tuple[Union[int, None]], None] = None,
+    gc_n_lags: int = 20,
+    n_jobs: int = 1,
+    verbose: Union[bool, str, int, None] = None,
+) -> Union[SpectralConnectivity, list[SpectralConnectivity]]:
+    """Compute frequency-domain multivariate connectivity measures.
+
+    The connectivity method(s) are specified using the "method" parameter. All
+    methods are based on estimates of the cross-spectral densities (CSD) Sxy.
+
+    Based on the "spectral_connectivity_epochs" function of the
+    "mne-connectivity" package.
+
+    PARAMETERS
+    ----------
+    data : BaseEpochs | array-like
+    -   Data to compute connectivity on. If array-like, must have the dimensions
+        [epochs x signals x timepoints].
+
+    indices : tuple of tuple of array-like of int
+    -   Two tuples of arrays with indices of connections for which to compute
+        connectivity.
+
+    names : list | None; default None
+    -   Names of the channels in the data. If "data" is an Epochs object, these
+        channel names will override those in the object.
+
+    method : str | list of str; default "mic"
+    -   Connectivity measure(s) to compute. These can be ['mic', 'mim', 'gc',
+        'net_gc', 'trgc', 'net_trgc'].
+
+    sfreq : float; default 6.283185307179586
+    -   Sampling frequency of the data. Only used if "data" is array-like.
+
+    mode : str; default "multitaper"
+    -   Cross-spectral estimation method. Can be 'fourier', 'multitaper', or
+        'cwt_wavelet'.
+
+    t0 : float; default 0.0
+    -   Time of the first sample relative to the onset of the epoch, in seconds.
+        Only used if "data" is an array.
+
+    tmin : float | None; default None
+    -   The time at which to start computing connectivity, in seconds. If None,
+        starts from the first sample.
+
+    tmax : float | None; default None
+    -   The time at which to stop computing connectivity, in seconds. If None,
+        ends with the final sample.
+
+    fmt_fmin : float; default 0.0
+    -   Minumum frequency of interest, in Hz. Only used if "mode" is 'fourier'
+        or 'multitaper'.
+
+    fmt_fmax : float; default infinity
+    -   Maximum frequency of interest, in Hz. Only used if "mode" is 'fourier'
+        or 'multitaper'.
+
+    cwt_freqs : list of float | None; default None
+    -   The frequencies of interest, in Hz. If "mode" is 'cwt_morlet', this
+        cannot be None. Only used if "mode" if 'cwt_morlet'.
+
+    fmt_n_fft : int | None; default None
+    -   Length of the FFT. If None, the exact number of samples between "tmin"
+        and "tmax" will be used. Only used if "mode" is 'fourier' or
+        'multitaper'.
+
+    cwt_use_fft : bool; default True
+    -   Whether to use FFT-based convolution to compute the wavelet transform.
+        Only used if "mode" is 'cwt_morlet'.
+
+    mt_bandwidth : float | None; default None
+    -   Bandwidth of the multitaper windowing function, in Hz. Only used if
+        "mode" if 'multitaper'.
+
+    mt_adaptive : bool; default False
+    -   Whether or not to use adaptive weights to combine the tapered spectra
+        into the power spectral density. Only used if "mode" if 'multitaper'.
+
+    mt_low_bias : bool; default True
+    -   Whether or not to only use tapers with over 90% spectral concentration
+        within the bandwidth. Only used if "mode" if 'multitaper'.
+
+    cwt_n_cycles : float | list of float; default 7.0
+    -   Number of cycles to use when constructing the Morlet wavelets. Can be a
+        single number, or one per frequency. Only used if "mode" if
+        'cwt_morlet'.
+
+    cwt_decim : int | slice; default 1
+    -   To redice memory usage, decimation factor during time-frequency
+        decomposition. Default to 1 (no decimation). If int, uses
+        tfr[..., ::"decim"]. If slice, used tfr[..., "decim"]. Only used if
+        "mode" is 'cwt_morlet'.
+
+    n_seed_components : tuple of int or None | None; default None
+    -   Dimensionality reduction parameter specifying the number of seed
+        components to extract from the single value decomposition of the seed
+        channels' data for each connectivity node. If None, or if an individual
+        entry is None, no dimensionality reduction is performed.
+
+    n_target_components : tuple of int or None | None; default None
+    -   Dimensionality reduction parameter specifying the number of target
+        components to extract from the single value decomposition of the target
+        channels' data for each connectivity node. If None, or if an individual
+        entry is None, no dimensionality reduction is performed.
+
+    gc_n_lags : int; default 20
+    -   The number of lags to use when computing the autocovariance sequence
+        from the cross-spectral density. Only used if "method" is 'gc',
+        'net_gc', 'trgc', or 'net_trgc'.
+
+    n_jobs : int; default 1
+    -   Number of jobs to run in parallel when computing the cross-spectral
+        density.
+
+    verbose : bool | str | int | None; default None
+    -   Whether or not to print information about the status of the connectivity
+        computations. See MNE's logging information for further details.
+
+    RETURNS
+    -------
+    results : SpectralConnectivity | list[SpectralConnectivity]
+    -   The connectivity results as a single SpectralConnectivity object (if
+        only one method is called) or a list of SpectralConnectivity objects (if
+        multiple methods are called, where each object is the results for the
+        corresponding entry in "method").
+    """
+    if n_jobs != 1:
+        parallel, my_epoch_spectral_connectivity, _ = \
+            parallel_func(_epoch_spectral_connectivity, n_jobs,
+                          verbose=verbose)
+
+    # format fmin and fmax and check inputs
+    if fmin is None:
+        fmin = -np.inf  # set it to -inf, so we can adjust it later
+
+    fmin = np.array((fmin,), dtype=float).ravel()
+    fmax = np.array((fmax,), dtype=float).ravel()
+    if len(fmin) != len(fmax):
+        raise ValueError('fmin and fmax must have the same length')
+    if np.any(fmin > fmax):
+        raise ValueError('fmax must be larger than fmin')
+
+    n_bands = len(fmin)
+
+    # assign names to connectivity methods
+    if not isinstance(method, (list, tuple)):
+        method = [method]  # make it a list so we can iterate over it
+
+    # handle connectivity estimators
+    (con_method_types, n_methods, accumulate_psd,
+     n_comp_args) = _check_estimators(method=method, mode=mode)
+
+    events = None
+    event_id = None
+    if isinstance(data, BaseEpochs):
+        names = data.ch_names
+        times_in = data.times  # input times for Epochs input type
+        sfreq = data.info['sfreq']
+
+        events = data.events
+        event_id = data.event_id
+
+        # Extract metadata from the Epochs data structure.
+        # Make Annotations persist through by adding them to the metadata.
+        metadata = data.metadata
+        if metadata is None:
+            annots_in_metadata = False
+        else:
+            annots_in_metadata = all(
+                name not in metadata.columns for name in [
+                    'annot_onset', 'annot_duration', 'annot_description'])
+        if hasattr(data, 'annotations') and not annots_in_metadata:
+            data.add_annotations_to_metadata(overwrite=True)
+        metadata = data.metadata
+    else:
+        times_in = None
+        metadata = None
+
+    # loop over data; it could be a generator that returns
+    # (n_signals x n_times) arrays or SourceEstimates
+    epoch_idx = 0
+    logger.info('Connectivity computation...')
+    warn_times = True
+    for epoch_block in _get_n_epochs(data, n_jobs):
+        if epoch_idx == 0:
+            # initialize everything times and frequencies
+            (n_cons, times, n_times, times_in, n_times_in, tmin_idx,
+             tmax_idx, n_freqs, freq_mask, freqs, freqs_bands, freq_idx_bands,
+             n_signals, indices_use, warn_times) = _prepare_connectivity(
+                epoch_block=epoch_block, times_in=times_in,
+                tmin=tmin, tmax=tmax, fmin=fmin, fmax=fmax, sfreq=sfreq,
+                indices=indices, mode=mode, fskip=fskip, n_bands=n_bands,
+                cwt_freqs=cwt_freqs, faverage=faverage)
+
+            # get the window function, wavelets, etc for different modes
+            (spectral_params, mt_adaptive, n_times_spectrum,
+             n_tapers) = _assemble_spectral_params(
+                mode=mode, n_times=n_times, mt_adaptive=mt_adaptive,
+                mt_bandwidth=mt_bandwidth, sfreq=sfreq,
+                mt_low_bias=mt_low_bias, cwt_n_cycles=cwt_n_cycles,
+                cwt_freqs=cwt_freqs, freqs=freqs, freq_mask=freq_mask)
+
+            # unique signals for which we actually need to compute PSD etc.
+            sig_idx = np.unique(np.r_[indices_use[0], indices_use[1]])
+
+            # map indices to unique indices
+            idx_map = [np.searchsorted(sig_idx, ind) for ind in indices_use]
+
+            # allocate space to accumulate PSD
+            if accumulate_psd:
+                if n_times_spectrum == 0:
+                    psd_shape = (len(sig_idx), n_freqs)
+                else:
+                    psd_shape = (len(sig_idx), n_freqs, n_times_spectrum)
+                psd = np.zeros(psd_shape)
+            else:
+                psd = None
+
+            # create instances of the connectivity estimators
+            con_methods = [mtype(n_cons, n_freqs, n_times_spectrum)
+                           for mtype in con_method_types]
+
+            sep = ', '
+            metrics_str = sep.join([meth.name for meth in con_methods])
+            logger.info('    the following metrics will be computed: %s'
+                        % metrics_str)
+
+        # check dimensions and time scale
+        for this_epoch in epoch_block:
+            _, _, _, warn_times = _get_and_verify_data_sizes(
+                this_epoch, sfreq, n_signals, n_times_in, times_in,
+                warn_times=warn_times)
+
+        call_params = dict(
+            sig_idx=sig_idx, tmin_idx=tmin_idx,
+            tmax_idx=tmax_idx, sfreq=sfreq, mode=mode,
+            freq_mask=freq_mask, idx_map=idx_map, block_size=block_size,
+            psd=psd, accumulate_psd=accumulate_psd,
+            mt_adaptive=mt_adaptive,
+            con_method_types=con_method_types,
+            con_methods=con_methods if n_jobs == 1 else None,
+            n_signals=n_signals, n_times=n_times,
+            accumulate_inplace=True if n_jobs == 1 else False)
+        call_params.update(**spectral_params)
+
+        if n_jobs == 1:
+            # no parallel processing
+            for this_epoch in epoch_block:
+                logger.info('    computing connectivity for epoch %d'
+                            % (epoch_idx + 1))
+                # con methods and psd are updated inplace
+                _epoch_spectral_connectivity(data=this_epoch, **call_params)
+                epoch_idx += 1
+        else:
+            # process epochs in parallel
+            logger.info('    computing connectivity for epochs %d..%d'
+                        % (epoch_idx + 1, epoch_idx + len(epoch_block)))
+
+            out = parallel(my_epoch_spectral_connectivity(
+                           data=this_epoch, **call_params)
+                           for this_epoch in epoch_block)
+            # do the accumulation
+            for this_out in out:
+                for _method, parallel_method in zip(con_methods, this_out[0]):
+                    _method.combine(parallel_method)
+                if accumulate_psd:
+                    psd += this_out[1]
+
+            epoch_idx += len(epoch_block)
+
+    # normalize
+    n_epochs = epoch_idx
+    if accumulate_psd:
+        psd /= n_epochs
+
+    # compute final connectivity scores
+    con = list()
+    for conn_method, n_args in zip(con_methods, n_comp_args):
+        # future estimators will need to be handled here
+        if n_args == 3:
+            # compute all scores at once
+            conn_method.compute_con(slice(0, n_cons), n_epochs)
+        elif n_args == 5:
+            # compute scores block-wise to save memory
+            for i in range(0, n_cons, block_size):
+                con_idx = slice(i, i + block_size)
+                psd_xx = psd[idx_map[0][con_idx]]
+                psd_yy = psd[idx_map[1][con_idx]]
+                conn_method.compute_con(con_idx, n_epochs, psd_xx, psd_yy)
+        else:
+            raise RuntimeError('This should never happen.')
+
+        # get the connectivity scores
+        this_con = conn_method.con_scores
+
+        if this_con.shape[0] != n_cons:
+            raise ValueError('First dimension of connectivity scores must be '
+                             'the same as the number of connections')
+        if faverage:
+            if this_con.shape[1] != n_freqs:
+                raise ValueError('2nd dimension of connectivity scores must '
+                                 'be the same as the number of frequencies')
+            con_shape = (n_cons, n_bands) + this_con.shape[2:]
+            this_con_bands = np.empty(con_shape, dtype=this_con.dtype)
+            for band_idx in range(n_bands):
+                this_con_bands[:, band_idx] =\
+                    np.mean(this_con[:, freq_idx_bands[band_idx]], axis=1)
+            this_con = this_con_bands
+
+        con.append(this_con)
+
+    freqs_used = freqs
+    if faverage:
+        # for each band we return the frequencies that were averaged
+        freqs = [np.mean(x) for x in freqs_bands]
+
+        # make sure freq_bands is a list of equal-length lists
+        # XXX: we lose information on which frequency points went into the
+        # computation. If h5netcdf supports numpy objects in the future, then
+        # we can change the min/max to just make it a list of lists.
+        freqs_used = freqs_bands
+        freqs_used = [[np.min(band), np.max(band)] for band in freqs_used]
+
+    if indices is None:
+        # return all-to-all connectivity matrices
+        # raveled into a 1D array
+        logger.info('    assembling connectivity matrix')
+        con_flat = con
+        con = list()
+        for this_con_flat in con_flat:
+            this_con = np.zeros((n_signals, n_signals) +
+                                this_con_flat.shape[1:],
+                                dtype=this_con_flat.dtype)
+            this_con[indices_use] = this_con_flat
+
+            # ravel 2D connectivity into a 1D array
+            # while keeping other dimensions
+            this_con = this_con.reshape((n_signals ** 2,) +
+                                        this_con_flat.shape[1:])
+            con.append(this_con)
+    # number of nodes in the original data,
+    n_nodes = n_signals
+
+    # create a list of connectivity containers
+    conn_list = []
+    for _con in con:
+        kwargs = dict(data=_con,
+                      names=names,
+                      freqs=freqs,
+                      method=method,
+                      n_nodes=n_nodes,
+                      spec_method=mode,
+                      indices=indices,
+                      n_epochs_used=n_epochs,
+                      freqs_used=freqs_used,
+                      times_used=times,
+                      n_tapers=n_tapers,
+                      metadata=metadata,
+                      events=events,
+                      event_id=event_id
+                      )
+        # create the connectivity container
+        if mode in ['multitaper', 'fourier']:
+            klass = SpectralConnectivity
+        else:
+            assert mode == 'cwt_morlet'
+            klass = SpectroTemporalConnectivity
+            kwargs.update(times=times)
+        conn_list.append(klass(**kwargs))
+
+    logger.info('[Connectivity computation done]')
+
+    if n_methods == 1:
+        # for a single method return connectivity directly
+        conn_list = conn_list[0]
+
+    return conn_list
