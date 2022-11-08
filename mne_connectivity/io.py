d98a5f2399659b8b734d7928e1567a0894c5255a
Daniel McCloy <dan@mccloy.info>
2022-11-08 15:19:51 -0600

    fix for loading with missing event_id

--- mne_connectivity/io.py
+++ mne_connectivity/io.py
@@ -44,10 +44,13 @@ def _xarray_to_conn(array, cls_func):
     metadata = _prepare_read_metadata(metadata)
 
     # write event IDs
-    event_id_keys = np.atleast_1d(array.attrs.pop('event_id_keys')).tolist()
-    event_id_vals = np.atleast_1d(array.attrs.pop('event_id_vals')).tolist()
-    event_id = {key: val for key, val in zip(event_id_keys, event_id_vals)}
-    array.attrs['event_id'] = event_id
+    if 'event_id_keys' in array.attrs and 'event_id_vals' in array.attrs:
+        event_id_keys = np.atleast_1d(
+            array.attrs.pop('event_id_keys')).tolist()
+        event_id_vals = np.atleast_1d(
+            array.attrs.pop('event_id_vals')).tolist()
+        event_id = dict(zip(event_id_keys, event_id_vals))
+        array.attrs['event_id'] = event_id
 
     # create the connectivity class
     conn = cls_func(
