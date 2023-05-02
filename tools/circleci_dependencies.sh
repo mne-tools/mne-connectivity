fb85c27acdc47c56b04fcfaeaeb83a1c323f3f86
Daniel McCloy <dan@mccloy.info>
2023-05-02 10:11:19 -0500

    fix foolishness

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -8,5 +8,5 @@ echo "Installing doc build dependencies"
 python -m pip uninstall -y pydata-sphinx-theme
 python -m pip install --upgrade --progress-bar off --only-binary matplotlib PyQt6 -r requirements.txt -r requirements_testing.txt -r requirements_doc.txt
 python -m pip install --upgrade --progress-bar off git+https://github.com/mne-tools/mne-python
-python -m pip install --progress-bar off git+https://github.com/sphinx-gallery/sphinx-gallery.git@master#egg=proj git+https://github.com/pyvista/pyvista.git@main#egg=proj git+https://github.com/pyvista/pyvistaqt.git@main#egg=proj
+python -m pip install --progress-bar off git+https://github.com/sphinx-gallery/sphinx-gallery.git@master#egg=sphinx_gallery git+https://github.com/pyvista/pyvista.git@main#egg=pyvista git+https://github.com/pyvista/pyvistaqt.git@main#egg=pyvistaqt
 python -m pip install -e .
