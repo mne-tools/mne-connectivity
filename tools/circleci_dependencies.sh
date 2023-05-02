220032bcd3542034a4aeda5ae8057238a7cafdf9
Daniel McCloy <dan@mccloy.info>
2023-05-02 10:04:24 -0500

    fix egg URLs in CircleCI setup

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -8,5 +8,5 @@ echo "Installing doc build dependencies"
 python -m pip uninstall -y pydata-sphinx-theme
 python -m pip install --upgrade --progress-bar off --only-binary matplotlib PyQt6 -r requirements.txt -r requirements_testing.txt -r requirements_doc.txt
 python -m pip install --upgrade --progress-bar off git+https://github.com/mne-tools/mne-python
-python -m pip install --progress-bar off https://github.com/sphinx-gallery/sphinx-gallery/zipball/master https://github.com/pyvista/pyvista/zipball/main https://github.com/pyvista/pyvistaqt/zipball/main
+python -m pip install --progress-bar off git+https://github.com/sphinx-gallery/sphinx-gallery.git@master#egg=proj git+https://github.com/pyvista/pyvista.git@main#egg=proj git+https://github.com/pyvista/pyvistaqt.git@main#egg=proj
 python -m pip install -e .
