6fe682fc7d2ea88266641fd5b823088d122acc6c
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-10-24 23:18:27 +0200

    bug fix missing refactoring for example

--- examples/handling_ragged_arrays.py
+++ examples/handling_ragged_arrays.py
@@ -14,7 +14,7 @@ numbers of seeds and targets can be handled in MNE-Connectivity.
 
 import numpy as np
 
-from mne_connectivity import spectral_connectivity_epochs
+from mne_connectivity import spectral_connectivity_epochs_multivariate
 
 ###############################################################################
 # Background
@@ -44,7 +44,7 @@ from mne_connectivity import spectral_connectivity_epochs
 #   targets = [[2, 3, 4], [4         ]]
 #
 # The ``indices`` parameter passed to
-# :func:`~mne_connectivity.spectral_connectivity_epochs` and
+# :func:`~mne_connectivity.spectral_connectivity_epochs_multivariate` and
 # :func:`~mne_connectivity.spectral_connectivity_time` must be a tuple of
 # array-likes, meaning
 # that the indices can be passed as a tuple of: lists; tuples; or NumPy arrays.
@@ -108,7 +108,7 @@ ragged_indices = ([[0, 1], [0, 1, 2, 3]],  # seeds
                   [[2, 3, 4], [4]])  # targets
 
 # compute connectivity
-con = spectral_connectivity_epochs(
+con = spectral_connectivity_epochs_multivariate(
     data, method='mic', indices=ragged_indices, sfreq=sfreq, fmin=10, fmax=30,
     verbose=False)
 patterns = np.array(con.attrs['patterns'])
